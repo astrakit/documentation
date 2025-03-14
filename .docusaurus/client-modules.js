eab9cf3aa820251163aaa2acefa5e01d0dc573ed
catpawz <cat@catpawz.net>
2025-03-14 17:38:56 +0100

    Intial commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/run/media/cat/QVO/01 - CODE/astrakit-docs/documentation/node_modules/infima/dist/css/default/default.css"),
+  require("/run/media/cat/QVO/01 - CODE/astrakit-docs/documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/run/media/cat/QVO/01 - CODE/astrakit-docs/documentation/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/run/media/cat/QVO/01 - CODE/astrakit-docs/documentation/src/css/custom.css"),
+];
